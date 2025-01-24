1fec35b3627352275751b5899b50f6cdfd897415
mckoh <michael.kohlegger@gmail.com>
2025-01-24 09:36:22 +0100

    prepared integration column

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,33 @@ import streamlit as st
 from viedoors import BSTLoader, FLTLoader, HMLoader, FMLoader
 from viedoors import CADLoader, NPALoader
 
-st.title("VIE-Doors Data Integrator")
+st.set_page_config(
+    page_title="VIE Door Integrator",
+    page_icon="📃",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None
+)
+
+st.title("VIE-Door Integrator")
 
 st.markdown("Unterhalb können die Files ausgewählt werden, die im weiteren Verlauf integriert werden. Bitte beachten Sie, dass alle 6 Files aufeinander abgestimmt sein, und Daten über dasselbe Objekt (z.B. 420) enthalten müssen.")
 
-bst = st.file_uploader("Sisando BST File", "xlsx")
-flt = st.file_uploader("Sisando FLT File", "xlsx")
-hm = st.file_uploader("Schrack HM File", "xls")
-npa = st.file_uploader("NPA File", "xlsx")
-fm = st.file_uploader("Filemaker File", "xlsx")
-cad = st.file_uploader("CAD File", "xlsx")
+col_1, col_2, col_3 = st.columns(3, gap="medium", vertical_alignment="top")
 
-if st.button("Load Data"):
+with col_1:
+    bst = st.file_uploader("Sisando BST File", "xlsx")
+    flt = st.file_uploader("Sisando FLT File", "xlsx")
+
+with col_2:
+    hm = st.file_uploader("Schrack HM File", "xls")
+    npa = st.file_uploader("NPA File", "xlsx")
+
+with col_3:
+    fm = st.file_uploader("Filemaker File", "xlsx")
+    cad = st.file_uploader("CAD File", "xlsx")
+
+if st.button("Load Data", type="primary"):
 
     if bst is not None and flt is not None and hm is not None and \
         npa is not None and fm is not None and cad is not None:
@@ -26,4 +41,3 @@ if st.button("Load Data"):
         cad_data = CADLoader(file=cad, title="FLT")
 
         st.write("All files successfully loaded.")
-        # st.dataframe(bst_data.get_data())
